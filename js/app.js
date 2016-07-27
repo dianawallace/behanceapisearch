38c9f878616e212e7be10a7c2ee73418f6032e28
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-07-27 22:16:37 +0000

    add css styling for artists profiles and projects

--- js/app.js
+++ js/app.js
@@ -25,129 +25,82 @@ $(function() {
             success: function getData(data) {
                 //console.log(data);
                 showResults(data.users);
-                
-            
-           
-            
-                
-                // console.log(data.users);
-                // if (data.ok) {
-                //     if (data.count > 0) {
-                //         //$.each is a higher order function. It takes an array and a function as an argument.
-                //         //The function is executed once for each item in the array.
-                //         $.each(data.result.items, function(i, item) {
-                //                 var popularArtists = showResults(item);
-
-                //                 $('.results').append(popularArtists);
-                //         });
-                //     }
-                //     else {
-                //         $('<p>No artists found.</p>').appendTo('.results');
-                //     }
-                // }
-                // else {
-                //     $('.results').empty();
-                //     //alert(data.error);
-                // }
+                //project_ajax(data.users);
             }
         });
     }); // end of form
 
-    //L.mapbox.accessToken = 'pk.eyJ1IjoiZGlhbmF3YWxsYWNlIiwiYSI6ImNpcWNmbDFkbzAyOG1mbG0xb3VyNnA1cXYifQ.MkDM9QfSVDxHas9Mk3f1pA';
-    //var map = L.mapbox.map('map', 'examples.map-h67hf2ic');
-
     // artists search on Behance 
     var showResults = function(artists) {
-
+        //console.log(project);
         // clone our result template code
         var result = $('.templates .user').clone();
 
         //console.log(artists);
         $.each(artists, function(i, u) {
-                console.log(u.display_name);
+                //console.log(u.display_name);
                 var the_images = u.images;
                 var user_name = u.username;
                 var the_name = u.display_name;
                 var the_fields = u.fields;
                 var the_location = u.location;
                 var the_stats = u.stats;
-                console.table(the_stats);
-                console.log(the_location);
+                // console.table(the_stats);
+                // console.log(the_location);
 
-                //var geocoder = L.mapbox.geocoder('mapbox.places');
-
-                //geocoder.query(the_location, showMap);
-
-                //function showMap(err, data) {
-                    // The geocoder can return an area, like a city, or a
-                    // point, like an address. Here we handle both cases,
-                    // by fitting the map bounds to an area or zooming to a point.
-                    //if (data.lbounds) {
-                        //map.fitBounds(data.lbounds);
-                    //}
-                    //else if (data.latlng) {
-                        //map.setView([data.latlng[0], data.latlng[1]], 13);
-                    //}
-
-                    //L.mapbox.featureLayer({
-                        // this feature is in the GeoJSON format: see geojson.org
-                        // for the full specification
-                        //type: 'Feature',
-                        //geometry: {
-                            //type: 'Point',
-                            // coordinates here are in longitude, latitude order because
-                            // x, y is the standard for GeoJSON and many formats
-                            //coordinates: [-77.03221142292,
-                                //38.913371603574
-                           // ]
-                        //},
-                        //properties: {
-                            //title: 'Peregrine Espresso',
-                            //description: '1718 14th St NW, Washington, DC',
-                            // one can customize markers by adding simplestyle properties
-                            // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-                            //'marker-size': 'large',
-                            //'marker-color': '#BE9A6B',
-                            //'marker-symbol': 'cafe'
-                        //}
-                    //}).addTo(map);
-                //}
                 //show artist results
                 var name_list = $('.names');
-                // implement map for each artist here
+                // implement artist information here
                 if (artist.toLowerCase() == u.display_name.toLowerCase()) {
                     var template = '<ul class="name_list"> <li> <img src = "' + the_images['138'] + '"/>' + '</li> <a target="_blank" href="https://www.behance.net/' + u.username + '">' +
                         the_name + ' </a></li>' + '<li> ' + the_fields + '</li>' + '<li> ' + the_location + '</li> ' + '<li> ' + 'Appreciations: ' + the_stats.appreciations + '</li> '+ '<li> ' + 'Followers: ' + the_stats.followers + '</li></ul>';
                     name_list.append(template);
                 }
-                projects(u.username);  
-                console.log(u.username);
+                get_projects(u.username);
                 
             });
-            
-            
-            
-            // show artists profile image
-            //result.find('.images').attr('src', artists.images);
-
-        //show artists fields in result
-        //result.find('.fields').text(artists.fields);
-
-        //show artists country in result
-        //result.find('.location').text(artists.location);
-
-        //show artists stats in result - followers and appreciations
-        //result.find('.stats').text(artists.stats);
-
         return result;
     };
-        function projects(username) {
-           $.getJSON('https://api.behance.net/v2/users/' + username + '/projects?client_id=FqBdMrhgGDU37dYsu4NfGTkW4gm3B1Yn&callback=?', function (projects){
-               console.log(projects.projects);
+        // show artist projects here
+    /*    function project_ajax(username) { 
+            
+        var project_url = 'https://api.behance.net/v2/users?' + '/projects?client_id=FqBdMrhgGDU37dYsu4NfGTkW4gm3B1Yn';
+        var parameters = {
+            dataType: 'jsonp',
+           client_id: 'FqBdMrhgGDU37dYsu4NfGTkW4gm3B1Yn',
+        };
+        $.ajax({
+            url: 'https://api.behance.net/v2/users/' + username + '/projects',
+            data: parameters,
+            type: "GET",
+            success: function getData(data) {
+                get_projects(data.users);
+            
+            }
+            */
+        //});
+     // end of form
+            //function get_projects(this_finally_works) {
+                //console.log(this_finally_works);
+           // }
+        function get_projects(username) {
+            $.getJSON('https://api.behance.net/v2/users/' + username + '/projects?client_id=FqBdMrhgGDU37dYsu4NfGTkW4gm3B1Yn&callback=?', function(projects){
+               console.log(projects.projects); 
                $.each(projects.projects, function(i, project){
-                console.log(project); 
-                $('body').append(project.id); 
+                console.log(projects); 
+                
+                var project_list = $('.projects');
+                var the_name = project.name;
+                var the_covers = project.covers['115'];
+                var the_owners = project.owners;
+                // console.log(the_name, the_covers);
+                
+                if (artist.toLowerCase() == project.owners[0].display_name.toLowerCase()) {
+                    var template = "<ul class='projects_list'><li><img src='" + project.covers['202'] + "'/>" + "</li><li>" + project.name + "</li></ul>";
+                $('.projects').append(template); 
+                }
                }); 
-           }); 
-        }
+            })
+            
+        }; //end of get project function
 }); // end of jQuery
